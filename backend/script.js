edead31102d5528d86d57d983bc6bdaf50a00514
Kyle <uwu.akane8@gmail.com>
2025-05-09 10:10:34 +0200

    Alot of changes
    - Navbar updates: hamburger menu on smaller screens,
    
    - Products Page: styling changes using media quary
    
    Rearrange of html file structure,

--- backend/script.js
+++ backend/script.js
@@ -1,3 +1,28 @@
 function sendMelding() {
     alert("Melding sendt!");
 }
+
+function darkmode() {
+    /* funker ikke :() */
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+document.getElementById('comment-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const input = document.getElementById('comment').value;
+    document.getElementById('comment-section').innerHTML += `<p>${input}</p>`;
+});
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) { // Adjust the scroll threshold as needed
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+}
